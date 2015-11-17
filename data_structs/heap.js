4f1a4e77721e7724a2de5739e5e7c0f5e20d8b1e
Jonathan Price <jonmarkprice@users.noreply.github.com>
2015-11-17 10:33:26 -0600

    Create heap.js

---
+++ data_structs/heap.js
@@ -0,0 +1,19 @@
+var heap = {
+  root: null;
+}
+
+function Node() {
+  this.root = null;
+  this.left = null;
+  this.right = null;
+}
+
+function insert(node) {
+  function recur(node, root) {
+    
+  }
+  if ( ) {}
+  else {
+  
+  }
+}
