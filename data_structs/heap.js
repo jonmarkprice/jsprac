59d7187076fee71e7c08b7c36db0f7cd496dc62c
Jonathan Price <jonmarkprice@users.noreply.github.com>
2015-11-17 13:18:52 -0600

    Update heap.js

--- data_structs/heap.js
+++ data_structs/heap.js
@@ -8,12 +8,23 @@ function Node() {
   this.right = null;
 }
 
-function insert(node) {
-  function recur(node, root) {
-    
-  }
-  if ( ) {}
-  else {
-  
+function Heap() {
+  this.root = null;
+  this.insert function(node) {
+    function recur(node, root) {
+      
+      if (root.left === null) {
+        root.left = node;
+        return true;
+      }
+      else if (root.right === null) {
+        root.right = node;
+        return true;
+      }
+      else {
+        return recur(node, root.left);
+      }
+    }
+    return recur(node, this.root)
   }
 }
